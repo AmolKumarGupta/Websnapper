ab813c7e61a44c61e090c32c6844bbc4d313abba
amol <amlo1232001@gmail.com>
2023-10-11 00:14:47 +0530

    refactor: update display to absolute

--- resources/js/Components/Design/LeftShade.jsx
+++ resources/js/Components/Design/LeftShade.jsx
@@ -1,5 +1,5 @@
 
 
 export default function LeftShade() {
-  return <div className="hidden md:block fixed left-[-100px] lg:left-[-200px] bg-gradient-to-b from-gray-200 to-gray-100 border-[1px] rounded-[59%_41%_100%_0%_/_0%_100%_0%_100%] border-gray-100  min-h-[600px] w-[200px] lg:w-[400px]"></div>
+  return <div className="hidden md:block absolute left-[-100px] lg:left-[-200px] bg-gradient-to-b from-gray-200 to-gray-100 border-[1px] rounded-[59%_41%_100%_0%_/_0%_100%_0%_100%] border-gray-100  min-h-[600px] w-[200px] lg:w-[400px]"></div>
 }
--- resources/js/Components/Design/RightShade.jsx
+++ resources/js/Components/Design/RightShade.jsx
@@ -1,5 +1,5 @@
 
 
 export default function RightShade() {
-  return <div className="hidden md:block fixed right-[-100px] lg:right-[-200px] bg-gradient-to-b from-gray-200 to-gray-100 border-[1px] rounded-[41%_59%_0%_100%_/_100%_0%_100%_0%] border-gray-100  min-h-[600px] w-[200px] lg:w-[400px]"></div>
+  return <div className="hidden md:block absolute right-[-100px] lg:right-[-200px] bg-gradient-to-b from-gray-200 to-gray-100 border-[1px] rounded-[41%_59%_0%_100%_/_100%_0%_100%_0%] border-gray-100  min-h-[600px] w-[200px] lg:w-[400px]"></div>
 }
