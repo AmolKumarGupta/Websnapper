adf31037e29481a8ed2674750a4904013183e04b
amol <amlo1232001@gmail.com>
2023-10-12 00:14:43 +0530

    refactor: setup roles

--- config/app.php
+++ config/app.php
@@ -168,6 +168,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        
+        Spatie\Permission\PermissionServiceProvider::class,
+
     ])->toArray(),
 
     /*
