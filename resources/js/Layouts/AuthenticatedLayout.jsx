4716edacbf35ad299abef94ee9a83c7e1e3f6ec7
amol <amlo1232001@gmail.com>
2023-10-15 00:23:54 +0530

    refactor: hidden header section

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -113,7 +113,7 @@ export default function Authenticated({ user, header, children }) {
                 </div>
             </nav>
 
-            {header && (
+            {(header && false) && (
                 <header className="bg-white shadow">
                     <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">{header}</div>
                 </header>
