2188348036f49aa0b50fc7dbb6e12ee12a484c87
amol <amlo1232001@gmail.com>
2023-10-18 23:09:08 +0530

    feat: add relationships

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Enums\VideoStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Video extends Model
 {
@@ -20,5 +21,10 @@ class Video extends Model
     protected $casts = [
         'status' => VideoStatus::class,
     ];
+
+    public function user(): BelongsTo 
+    {
+        return $this->belongsTo(User::class);
+    }
     
 }
