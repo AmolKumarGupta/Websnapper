c0259f471abe35cf52fbcf007faed3cbd2a3acd9
amol <amlo1232001@gmail.com>
2023-11-25 00:06:35 +0530

    feat: create video_views table, resolves #70

---
+++ database/migrations/2023_11_24_182338_create_video_views_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('video_views', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('video_id')->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->string('model_type')->nullable();
+            $table->unsignedBigInteger('model_id')->nullable();
+            $table->string('email')->nullable()->index();
+            $table->timestamps();
+
+            $table->index(['model_type', 'model_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('video_views');
+    }
+};
