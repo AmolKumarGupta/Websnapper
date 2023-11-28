2fd143fb9eefe15b758091ddf6302a00599cb481
amol <amlo1232001@gmail.com>
2023-11-29 00:31:05 +0530

    test: share video, resolves #69

--- tests/Feature/Video/VideoTest.php
+++ tests/Feature/Video/VideoTest.php
@@ -11,15 +11,24 @@ class VideoTest extends TestCase
 {
     use RefreshDatabase;
 
+    protected static User $user;
+
+    protected function setUp(): void 
+    {
+        parent::setUp();
+
+        self::$user = User::factory()
+        ->has(
+            Video::factory()
+                ->count(1)
+                ->state(fn(array $attributes, User $user) => ['fk_user_id' => $user->id])
+        )
+        ->create();
+    }
+
     public function test_user_can_see_video() 
     {
-        $user = User::factory()
-            ->has(
-                Video::factory()
-                    ->count(1)
-                    ->state(fn(array $attributes, User $user) => ['fk_user_id' => $user->id])
-            )
-            ->create();
+        $user = self::$user;
 
         $user->videos->each(function ($v) use ($user) {
             $videoLink = hashget($v->id);
@@ -31,13 +40,7 @@ class VideoTest extends TestCase
 
     public function test_unauthorized_cannot_see_video() 
     {
-        $user = User::factory()
-            ->has(
-                Video::factory()
-                    ->count(1)
-                    ->state(fn(array $attributes, User $user) => ['fk_user_id' => $user->id])
-            )
-            ->create();
+        $user = self::$user;
 
         $unAuthorizedUser = User::factory()->create();
 
@@ -46,6 +49,25 @@ class VideoTest extends TestCase
     
             $response = $this->actingAs($unAuthorizedUser)->get("/videos/$videoLink");
             $response->assertStatus(403);
+        });
+    }
+
+    public function test_authorized_can_see_video() 
+    {
+        $user = self::$user;
+
+        $authorizedUser = User::factory()->create();
+
+        $user->videos->each(function ($v) use ($user, $authorizedUser) {
+            $videoLink = hashget($v->id);
+
+            $this->actingAs($user)->post('/video/access', [
+                "videoId" => $v->id,
+                "userEmail" => $authorizedUser->email,
+            ]);
+    
+            $response = $this->actingAs($authorizedUser)->get("/videos/$videoLink");
+            $response->assertStatus(200);
         });
     }
 
