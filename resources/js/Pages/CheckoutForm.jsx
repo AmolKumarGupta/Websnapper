075344a985bba22e43e5f83d3f41a4fb4720f5f5
amol <amlo1232001@gmail.com>
2023-12-16 17:44:52 +0530

    fix: add return_url using route(), #87

--- resources/js/Pages/CheckoutForm.jsx
+++ resources/js/Pages/CheckoutForm.jsx
@@ -55,7 +55,7 @@ export default function CheckoutForm() {
         const { error } = await stripe.confirmPayment({
             elements,
             confirmParams: {
-                return_url: "http://localhost:8000",
+                return_url: route('dashboard'),
             },
         });
 
