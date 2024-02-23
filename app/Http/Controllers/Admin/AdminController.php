38e198e9018337ea865f68dda424a8efe56b01c6
amol <amlo1232001@gmail.com>
2024-02-24 01:13:20 +0530

    feat: add transaction table, resolves #90

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Admin;
 
 use App\Enums\PaymentStatus;
 use App\Http\Controllers\Controller;
+use App\Models\Charge;
 use App\Models\Payment;
 use App\Models\User;
 use App\Models\Video;
 use App\Models\VideoView;
 use App\Utility\Stat;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller 
@@ -28,6 +30,22 @@ class AdminController extends Controller
                             ->sum('amount') / 100;
                     }),
             ),
+            'transactions' => Charge::with('user')->latest()
+                ->limit(5)->get()
+                ->map(function ($charge) {
+                    $detail = json_decode($charge->detail, true);
+
+                    return [
+                        "name" => ucfirst($charge->user->name),
+                        "amount" => "Rs " . $charge->amount / 100,
+                        "date" => Carbon::parse($charge->created_at)->diffForHumans(),
+                        "status" => ($charge->status==PaymentStatus::Succeeded->value) ? "paid" : "unpaid",
+                        "account" => $detail['brand'] ?? "",
+                        "accountNumber" => $detail['last4'] ?? "",
+                        "expiry" => ($detail['exp_month'] ?? "") ."/". ($detail['exp_year'] ?? ""),
+                    ];
+                })
+                ->toArray()
         ]);
     }
 }
