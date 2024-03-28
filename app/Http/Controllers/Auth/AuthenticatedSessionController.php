a6f8cf6e2ba59b9e7cab4682bb36ef460c3e1cd4
amol <amlo1232001@gmail.com>
2024-03-29 01:26:40 +0530

    feat: add logic for google signup

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Google\Client;
 use Inertia\Inertia;
 use Inertia\Response;
+use Google\Service\Drive;
+use Google\Service\Oauth2;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
@@ -57,4 +60,42 @@ class AuthenticatedSessionController extends Controller
 
         return Redirect::to('/');
     }
+
+    public function oauth(Request $request) {
+        $client = new Client();
+        $client->setAuthConfig(storage_path("app/private/google_cred.json"));
+        $client->setAccessType("offline");
+
+        $client->addScope(Drive::DRIVE);
+        $client->addScope(Oauth2::USERINFO_EMAIL);
+        $client->addScope(Oauth2::USERINFO_PROFILE);
+
+        $client->setRedirectUri(route('oauth.callback'));
+
+        $auth_url = $client->createAuthUrl();
+        header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
+        exit;
+    }
+
+    public function callback(Request $request) {
+        $request->validate([
+            "code" => "required",
+            "scope" => "required",
+        ]);
+        
+        $client = new Client();
+        $client->setAuthConfig(storage_path("app/private/google_cred.json"));
+        $client->setAccessType("offline");
+
+        $client->addScope(Drive::DRIVE);
+        $client->addScope(Oauth2::USERINFO_EMAIL);
+        $client->addScope(Oauth2::USERINFO_PROFILE);
+
+        $client->setRedirectUri(route('oauth.callback'));
+        $token = $client->fetchAccessTokenWithAuthCode($request->get('code'));
+
+        $service = new Oauth2($client);
+        $data = $service->userinfo->get();
+    }
+
 }
