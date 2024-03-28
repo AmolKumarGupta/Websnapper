d9d3138009fe34cf95932580aef2b65057f39e19
amol <amlo1232001@gmail.com>
2024-03-29 00:14:11 +0530

    fix: intellisense warnings

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -2,15 +2,16 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests\Auth\LoginRequest;
-use App\Providers\RouteServiceProvider;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Inertia\Response;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Route;
+use App\Providers\RouteServiceProvider;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Support\Facades\Redirect;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -34,11 +35,13 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        if (auth()->user()->hasRole('admin')) {
-            return redirect()->intended(RouteServiceProvider::ADMIN);
+        /** @var \App\Models\User */
+        $user = auth()->user();
+        if ($user->hasRole('admin')) {
+            return Redirect::intended(RouteServiceProvider::ADMIN);
         }
 
-        return redirect()->intended(RouteServiceProvider::HOME);
+        return Redirect::intended(RouteServiceProvider::HOME);
     }
 
     /**
@@ -52,6 +55,6 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        return Redirect::to('/');
     }
 }
