308c9ac2dd0589d05b715d942d08a9bff2df3f8a
amol <amlo1232001@gmail.com>
2024-09-26 15:08:23 +0530

    feat: folder migration

---
+++ database/migrations/2024_09_26_092949_create_folders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('folders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::table('videos', function (Blueprint $table) {
+            $table->unsignedBigInteger('folder_id')->nullable()->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('folders');
+
+        Schema::table('videos', function (Blueprint $table) {
+            $table->dropColumn('folder_id');
+        });
+    }
+};
